40957722104b5b945eb239f548aeefbe24b8cdd6
Artur Santana Pereira <112443860+artursantana@users.noreply.github.com>
2023-02-04 10:51:13 +0000

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,67 @@
+//VARIÁVEIS e seleções de Elementos
+const apiKey = '60ce963bee1266388d79b3083b85c681'
+const apiCountryURL = 'https://countryflagsapi.com/png/'
+
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector('#description')
+const weatherIconElement = document.querySelector('#weather-icon')
+const countryElement = document.querySelector('#country')
+const humidityElement = document.querySelector('#humidity')
+const windElement = document.querySelector('#wind span')
+
+const weatherContainer = document.querySelector('#weather-data')
+
+
+
+//Funções
+
+const getWeatherData = async(city) => {
+    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherUrl)
+    const data = await res.json()
+    return data
+}
+
+
+
+const showWeatherData = async (city) => {
+   const data = await getWeatherData(city)
+
+cityElement.innerText = data.name
+tempElement.innerText = parseInt(data.main.temp)
+descElement.innerText = data.weather[0].description
+weatherIconElement.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+countryElement.setAttribute('src', apiCountryURL + data.sys.country)
+humidityElement.innerText = `${data.main.humidity}%`;
+windElement.innerText = `${data.wind.speed}km/h`
+
+weatherContainer.classList.remove('hide')
+
+}
+
+//Eventos
+
+searchBtn.addEventListener('click',(e) =>{
+    e.preventDefault()
+
+    const city = cityInput.value;
+
+    showWeatherData(city)
+
+})
+
+
+cityInput.addEventListener('keyup', (e) => {
+    
+    if(e.code === 'Enter') {
+        const city = e.target.value
+        showWeatherData(city)
+    }
+    
+})
